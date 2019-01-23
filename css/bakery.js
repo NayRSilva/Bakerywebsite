6a14a63736f0a0e50c71759e9fd7e9975c51d341
NayRSilva <nayarasilva@cjr.org.br>
2019-01-23 16:31:39 -0200

    enviando os arquivos iniciais

---
+++ css/bakery.js
@@ -0,0 +1,71 @@
+
+    
+
+function initComparison(){
+    var x, i;
+    x= document.getElementsByClassName("resize");
+    for(i=0; i< x.length; i++){
+        compareImages(x[i]);
+    }
+
+
+function compareImages(img){
+    var slider, img, clicked = 0, w, h;
+    w = img.offsetWidth;
+    h = img.offsetHeight;
+
+    img.style.width = (w/2) + "px";
+
+    slider= document.createElement("DIV");
+    slider.setAttribute("class", "handle");
+    img.parentElement.insertBefore(slider, img);
+    slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
+    slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
+    slider.addEventListener("mousedown", slideReady);
+    window.addEventListener("mouseup", slideFinish);
+    function slideReady(e) {
+        e.preventDefault();
+        /* The slider is now clicked and ready to move: */
+        clicked = 1;
+        window.addEventListener("mousemove", slideMove);
+        window.addEventListener("touchmove", slideMove);
+      }
+      function slideFinish() {
+        /* The slider is no longer clicked: */
+        clicked = 0;
+      }
+      function slideMove(e) {
+        var pos;
+        /* If the slider is no longer clicked, exit this function: */
+        if (clicked == 0) return false;
+        /* Get the cursor's x position: */
+        pos = getCursorPos(e)
+        /* Prevent the slider from being positioned outside the image: */
+        if (pos < 0) pos = 0;
+        if (pos > w) pos = w;
+        /* Execute a function that will resize the overlay image according to the cursor: */
+        slide(pos);
+      }
+      function getCursorPos(e) {
+        var a, x = 0;
+        e = e || window.event;
+        /* Get the x positions of the image: */
+        a = img.getBoundingClientRect();
+        /* Calculate the cursor's x coordinate, relative to the image: */
+        x = e.pageX - a.left;
+        /* Consider any page scrolling: */
+        x = x - window.pageXOffset;
+        return x;
+      }
+      function slide(x) {
+        /* Resize the image: */
+        img.style.width = x + "px";
+        /* Position the slider: */
+        slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+      }
+    }
+}
+initComparison();
+
+    
+
